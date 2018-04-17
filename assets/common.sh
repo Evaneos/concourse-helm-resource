27a7e1093fe037a9dfb5122e45133930bedf7ff4
Régis Belson <regis@evaneos.com>
2018-04-17 15:38:14 +0200

    Allow to use kubeconfig

--- assets/common.sh
+++ assets/common.sh
@@ -2,56 +2,88 @@
 set -e
 
 setup_kubernetes() {
-  payload=$1
-  source=$2
+  local payload=$1
 
-  mkdir -p /root/.kube
-  kubeconfig_path=$(jq -r '.params.kubeconfig_path // ""' < $payload)
-  absolute_kubeconfig_path="${source}/${kubeconfig_path}"
-  if [ -f "$absolute_kubeconfig_path" ]; then
-    cp "$absolute_kubeconfig_path" "/root/.kube/config"
-  else
-    # Setup kubectl
-    cluster_url=$(jq -r '.source.cluster_url // ""' < $payload)
-    if [ -z "$cluster_url" ]; then
-      echo "invalid payload (missing cluster_url)"
+  export KUBECONFIG=$(mktemp $TMPDIR/kubernetes-resource-kubeconfig.XXXXXX)
+
+  # Optional. The path of kubeconfig file
+  local kubeconfig_file="$(jq -r '.params.kubeconfig_file // ""' < $payload)"
+  # Optional. The content of kubeconfig
+  local kubeconfig="$(jq -r '.source.kubeconfig // ""' < $payload)"
+
+  if [[ -n "$kubeconfig_file"  ]]; then
+    if [[ ! -f "$kubeconfig_file" ]]; then
+      echoerr "kubeconfig file '$kubeconfig_file' does not exist"
       exit 1
     fi
-    if [[ "$cluster_url" =~ https.* ]]; then
 
-      cluster_ca=$(jq -r '.source.cluster_ca // ""' < $payload)
-      admin_key=$(jq -r '.source.admin_key // ""' < $payload)
-      admin_cert=$(jq -r '.source.admin_cert // ""' < $payload)
-      token=$(jq -r '.source.token // ""' < $payload)
-      token_path=$(jq -r '.params.token_path // ""' < $payload)
+    cat "$kubeconfig_file" > $KUBECONFIG
+  elif [[ -n "$kubeconfig" ]]; then
+    echo "$kubeconfig" > $KUBECONFIG
+  else
+    # Optional. The address and port of the API server. Requires token.
+    local server="$(jq -r '.source.server // ""' < $payload)"
+    # Optional. Bearer token for authentication to the API server. Requires server.
+    local token="$(jq -r '.source.token // ""' < $payload)"
+    # Optional. The namespace scope. Defaults to default if doesn't specify in kubeconfig.
+    local namespace="$(jq -r '.source.namespace // ""' < $payload)"
+    # Optional. A certificate file for the certificate authority.
+    local certificate_authority="$(jq -r '.source.certificate_authority // ""' < $payload)"
+    # Optional. If true, the API server's certificate will not be checked for
+    # validity. This will make your HTTPS connections insecure. Defaults to false.
+    local insecure_skip_tls_verify="$(jq -r '.source.insecure_skip_tls_verify // ""' < $payload)"
 
-      ca_path="/root/.kube/ca.pem"
-      echo "$cluster_ca" | base64 -d > $ca_path
-      kubectl config set-cluster default --server=$cluster_url --certificate-authority=$ca_path
-
-      if [ -f "$source/$token_path" ]; then
-        kubectl config set-credentials admin --token=$(cat $source/$token_path)
-      elif [ ! -z "$token" ]; then
-        kubectl config set-credentials admin --token=$token
-      else
-        mkdir -p /root/.kube
-        key_path="/root/.kube/key.pem"
-        cert_path="/root/.kube/cert.pem"
-        echo "$admin_key" | base64 -d > $key_path
-        echo "$admin_cert" | base64 -d > $cert_path
-        kubectl config set-credentials admin --client-certificate=$cert_path --client-key=$key_path
-      fi
-
-      kubectl config set-context default --cluster=default --user=admin
-    else
-      kubectl config set-cluster default --server=$cluster_url
-      kubectl config set-context default --cluster=default
+    if [[ -z "$server" || -z "$token" ]]; then
+      echoerr 'You must specify "server" and "token", if not specify "kubeconfig".'
+      exit 1
     fi
 
-    kubectl config use-context default
+    local -r AUTH_NAME=auth
+    local -r CLUSTER_NAME=cluster
+    local -r CONTEXT_NAME=kubernetes-resource
+
+    # Build options for kubectl config set-credentials
+    # Avoid to expose the token string by using placeholder
+    local set_credentials_opts="--token=**********"
+    exe kubectl config set-credentials $AUTH_NAME $set_credentials_opts
+    # placeholder is replaced with actual token string
+    sed -i -e "s/[*]\{10\}/$token/" $KUBECONFIG
+
+    # Build options for kubectl config set-cluster
+    local set_cluster_opts="--server=$server"
+    if [[ -n "$certificate_authority" ]]; then
+      local ca_file=$(mktemp $TMPDIR/kubernetes-resource-ca_file.XXXXXX)
+      echo -e "$certificate_authority" > $ca_file
+      set_cluster_opts="$set_cluster_opts --certificate-authority=$ca_file"
+    fi
+    if [[ "$insecure_skip_tls_verify" == "true" ]]; then
+      set_cluster_opts="$set_cluster_opts --insecure-skip-tls-verify"
+    fi
+    exe kubectl config set-cluster $CLUSTER_NAME $set_cluster_opts
+
+    # Build options for kubectl config set-context
+    local set_context_opts="--user=$AUTH_NAME --cluster=$CLUSTER_NAME"
+    if [[ -n "$namespace" ]]; then
+      set_context_opts="$set_context_opts --namespace=$namespace"
+    fi
+    exe kubectl config set-context $CONTEXT_NAME $set_context_opts
+
+    exe kubectl config use-context $CONTEXT_NAME
   fi
 
-  kubectl version
+  # Optional. The name of the kubeconfig context to use.
+  local context="$(jq -r '.source.context // ""' < $payload)"
+  if [[ -n "$context" ]]; then
+    exe kubectl config use-context $context
+  fi
+
+  # Display the client and server version information
+  exe kubectl version
+
+  # Ignore the error from `kubectl cluster-info`. From v1.9.0, this command
+  # fails if it cannot find the cluster services.
+  # See https://github.com/kubernetes/kubernetes/commit/998f33272d90e4360053d64066b9722288a25aae
+  exe kubectl cluster-info 2>/dev/null ||:
 }
 
 setup_tls() {
@@ -247,4 +279,9 @@ setup_resource() {
   setup_tls $1
   setup_helm $1 $2
   setup_repos $1
+}
+
+# exe executes the command after printing the command trace to stdout
+exe() {
+  echo "+ $*"; "$@"
 }
